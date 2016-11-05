a305882c29b5be8c341018c213c39ca81c87b119
Stefan <kh2ouija@yahoo.com>
2016-11-05 18:50:41 +0200

    cors on controller

--- src/main/java/com/travelcoin/controller/UserController.java
+++ src/main/java/com/travelcoin/controller/UserController.java
@@ -11,6 +11,7 @@ import com.travelcoin.repository.UserRepository;
 import com.travelcoin.service.AgentsFacade;
 import com.travelcoin.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     @Autowired
